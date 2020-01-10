0d141bf17d0aba96f4cb345775ea2dc2a660e600
Michael Olegovich <miken@datasunrise.com>
2020-01-10 09:49:30 +0300

    -- uncsry

--- temp/fstl-master/gl/quad.frag
+++
@@ -1,7 +0,0 @@
-#version 120
-
-varying vec3 frag_color;
-
-void main() {
-    gl_FragColor = vec4(frag_color, 1.0);
-}
--- temp/fstl-master/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-
-#include "app.h"
-
-int main(int argc, char *argv[])
-{
-    App a(argc, argv);
-    return a.exec();
-}
