b7c73ee3bb4e2b1e94068f5450dddbf49d5f3de2
Michael O. Nikonov <wcdnail@gmail.com>
2020-02-19 18:09:26 +0300

    ++

--- twins2/main-twins-second.cpp
+++ twins2/main-twins-second.cpp
@@ -2,13 +2,16 @@
 #include "main-twins-second.h"
 #include <iostream>
 #include <iomanip>
+#include <filesystem>
 
 int main(int argc, const char *argv[], const char *envp[])
 {
     try {
+        std::filesystem::directory_iterator it;
     }
     catch(std::exception &ex) {
         std::cerr << "ERROR: " << ex.what() << std::endl;
     }
     return 0;
 }
+
