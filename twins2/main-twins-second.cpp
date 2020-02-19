f0b85ec324fd80c618206be78f034f5d2a13faa4
Michael O. Nikonov <wcdnail@gmail.com>
2020-02-19 08:02:24 +0300

    ++

--- twins2/main-twins-second.cpp
+++ twins2/main-twins-second.cpp
@@ -1,7 +1,14 @@
 #include "stdafx.h"
 #include "main-twins-second.h"
+#include <iostream>
+#include <iomanip>
 
 int main(int argc, const char *argv[], const char *envp[])
 {
+    try {
+    }
+    catch(std::exception &ex) {
+        std::cerr << "ERROR: " << ex.what() << std::endl;
+    }
     return 0;
 }
